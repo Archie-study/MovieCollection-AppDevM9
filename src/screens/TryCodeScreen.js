ebe85e8705a03b3f5415068dfd24b17fc4fd4b08
Archie-study <archiestudy12@gmail.com>
2024-08-05 14:19:50 +0700

    Meeting 9 - Challenge 1 and Applied StyleSheet

--- src/screens/TryCodeScreen.js
+++ src/screens/TryCodeScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, Text, Image, FlatList } from 'react-native'
+import { View, Text, Image, FlatList, StyleSheet, ScrollView } from 'react-native'
 import { userData } from "../../data/TryCodeData";
 
 const TryCodeScreen = () => {
     return (
         <View style={{ flex: 1 }}>
+            <Text>An array of objects lets you store multiple values in a single variable. It stores a fixed-size sequential collection of elements of the same type. An array is used to store a collection of data, but it is often more useful to think of an array as a collection of variables of the same type.</Text>
             <FlatList 
                 contentContainerStyle={{ padding: 8}}
                 data={userData}
@@ -12,13 +13,13 @@ const TryCodeScreen = () => {
                 renderItem = {({ item }) => {
                     return (
                         <View
-                            style={{
-                                margin: 8,
-                                backgroundColor: 'lavender',
-                                borderWidth: 1
-                            }}
+                            style={styles.containerAnItem}
+                            // {{  margin: 8, backgroundColor: 'lavender', borderWidth: 1 }}
                         >
+                            <Image source={{uri: item.imageLink}} style={{width: 100, height: 100}} />
                             <Text>{item.name}</Text>
+                            <Text>{item.gender}</Text>
+                            <Text>{item.age}</Text>
                         </View>
                     )
                 }}
@@ -26,5 +27,13 @@ const TryCodeScreen = () => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    containerAnItem: {
+        margin: 8,
+        backgroundColor: 'lavender',
+        borderWidth: 1,
+    }
+})
 
 export default TryCodeScreen;
