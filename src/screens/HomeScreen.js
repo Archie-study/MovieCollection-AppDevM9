af1f85a8fc07fa0554c895ba94a8085875c8441e
Archie-study <archiestudy12@gmail.com>
2024-08-05 15:07:04 +0700

    Meeting 9 - Challenge 2 & DIY

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { View, Text, FlatList, StyleSheet, Image } from 'react-native'
+import { movieData } from "../../data/MovieData";
+
+const HomeScreen = () => {
+    return (
+        <View style={StyleSheet.mainContainer}>
+            <FlatList 
+                data={movieData}
+                keyExtractor={(item) => item.id}
+                contentContainerStyle={styles.flatListContainer}
+                renderItem={({item}) => {
+                    return (
+                        <View style={styles.dataContainer}>
+                            <Image 
+                                style={styles.movieImage}
+                                source={{ uri: item.imageLink }}
+                            />
+                            <View style={styles.movieDescriptionContainer}>
+                                <Text style={styles.title}>{item.title}</Text>
+                                <View style={styles.yearContainer}>
+                                    <Text>{item.year}</Text>
+                                </View>
+                                <Text>{item.rating}</Text>
+                            </View>
+                        </View>
+                    )
+                }}
+            
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1
+    },
+    flatListContainer: {
+        padding: 8
+    },
+    movieImage:{
+        width: 130,
+        height: 200,
+        borderRadius: 10
+    },
+    dataContainer:{
+        margin: 8,
+        borderColor: '#96ceb4',
+        borderWidth: 2, 
+        borderRadius: 10,
+        padding: 16,
+        flexDirection: 'row'
+    },
+    title:{
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    movieDescriptionContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        marginLeft: 8
+    },
+    yearContainer: {
+        marginTop: 8,
+        marginBottom: 8
+    }
+
+})
+
+export default HomeScreen;
